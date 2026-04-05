2e8b3faef83b90d33c1edd5606448fb72073d815
Boyan Balkanski <b@nsh.one>
2026-04-05 11:02:59 +0300

    fix: Update import statements for Section component in BlogPostForm and UserForm

--- app/Filament/Resources/BlogPosts/Schemas/BlogPostForm.php
+++ app/Filament/Resources/BlogPosts/Schemas/BlogPostForm.php
@@ -5,9 +5,9 @@ namespace App\Filament\Resources\BlogPosts\Schemas;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Str;
 
