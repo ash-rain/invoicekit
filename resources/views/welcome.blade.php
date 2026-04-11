0d228b33ecb53a808462151b0e527a89e5af8367
Boyan Balkanski <b@nsh.one>
2026-04-12 01:19:56 +0300

    fix: restore FAQ to native details/summary with custom CSS classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -679,17 +679,10 @@
         <div class="faq-list">
             @php $faqs = range(1, 11); @endphp
             @foreach ($faqs as $i)
-            <div x-data="{ open: false }" class="border-b border-gray-100">
-                <button @click="open = !open" class="w-full text-left py-5 flex justify-between items-center">
-                    <span class="font-semibold text-[#0f1117]">{{ $g("faq{$i}_q") }}</span>
-                    <svg class="w-5 h-5 text-gray-400 transition-transform" :class="{ 'rotate-180': open }" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
-                    </svg>
-                </button>
-                <div x-show="open" x-collapse class="pb-5 text-gray-500 text-sm leading-relaxed">
-                    {!! $gnl("faq{$i}_a") !!}
-                </div>
-            </div>
+            <details class="fq">
+                <summary class="fq-q">{!! $g("faq{$i}_q") !!}</summary>
+                <div class="fq-a">{!! $gnl("faq{$i}_a") !!}</div>
+            </details>
             @endforeach
         </div>
     </section>
